581862600b8e3b88a8edead391e643ba681290cf
deliagrigorita <deliagrigorita@yahoo.com>
2022-12-20 12:38:35 +0200

    Object Properties Menu

---
+++ Room Design/Assets/Scripts/MenuItemScript.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuItemScript : MonoBehaviour
+{
+    public Color hoverColor;
+    public Color baseColor;
+    public Image background;
+
+
+void Start()
+{
+    background.color = baseColor;
+}
+
+public void Select()
+{
+    background.color = hoverColor;
+}
+
+public void Deselect()
+{
+    background.color = baseColor
+}
+
+}
