33441100d734981476365fd7bf3ee548b5f5177a
Mihai Minea <mineamihai2001@gmail.com>
2023-01-09 18:44:54 +0200

    audio+audio menu

---
+++ Room Design/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
